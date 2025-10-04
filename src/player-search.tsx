a048c0763ae3408308dde9c73c4f6c8acb05cc2a
Daniyal Master <daniyalmaster693@gmail.com>
2025-10-04 09:44:08 -0400

    Created the player results view

--- src/player-search.tsx
+++ src/player-search.tsx
@@ -1,3 +1,30 @@
-import { Detail, List, Color, Icon, Action, ActionPanel } from "@raycast/api";
+import { List, Icon, Action, ActionPanel, LaunchProps } from "@raycast/api";
+import getPlayerResults from "./utils/getPlayerResults";
 
-export default function playerSearch() {}
+export default function PlayerSearch(props: LaunchProps<{ arguments: Arguments.PlayerSearch }>) {
+  const { playerSearchData, playerSearchLoading, playerSearchRevalidate } = getPlayerResults(props);
+  const playerResults = playerSearchData?.results[0].contents;
+
+  return (
+    <List navigationTitle="Search Results" searchBarPlaceholder="Search for a player" isLoading={playerSearchLoading}>
+      {playerResults?.map((player: any) => (
+        <List.Item
+          key={player.id}
+          title={player.displayName}
+          icon={player.image?.default || Icon.Person}
+          subtitle={player.description || ""}
+          actions={
+            <ActionPanel>
+              <Action
+                title="Refresh"
+                icon={Icon.ArrowClockwise}
+                onAction={playerSearchRevalidate}
+                shortcut={{ modifiers: ["cmd"], key: "r" }}
+              />
+            </ActionPanel>
+          }
+        />
+      ))}
+    </List>
+  );
+}
